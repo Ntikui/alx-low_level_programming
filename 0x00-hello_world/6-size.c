a99e80c5465dbff2eb75c306bb9665fdfdecaba1
Ntikui <veronicakorio@gmail.com>
2023-02-17 01:38:18 -0800

    5-printf.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#iclude <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: This function prints a message using the printf() function
+ * Return: Always 0 (Success)
+ */
+
+int maim(void)
+{
+printf(" Size of a char: 1 byte(s)
+Size of an int: 4 byte(s)
+Size of a long int: 4 byte(s)
+Size of a long long int: 8 byte(s)
+Size of a float: 4 byte(s)");
+}
