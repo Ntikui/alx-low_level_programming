dc7e74b1d81f9e5afbbefcfdf43e2c09e01f0e44
Ntikui <veronicakorio@gmail.com>
2023-02-17 01:47:55 -0800

    5-printf.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,8 +9,12 @@
 int main(void)
 {
 printf(" Size of a char: 1 byte(s)
+| ^
 Size of an int: 4 byte(s)
+| ^
 Size of a long int: 4 byte(s)
+| ^
 Size of a long long int: 8 byte(s)
+| ^
 Size of a float: 4 byte(s)");
 }
