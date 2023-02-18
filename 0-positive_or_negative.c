fb0ded92fa032bcc13dbf05e22120b8cca539dc1
Ntikui <veronicakorio@gmail.com>
2023-02-18 00:42:01 -0800

    0-positive_or_negative.c

---
+++ 0-positive_or_negative.c
@@ -0,0 +1,18 @@
+#include <atdio.h>
+
+/**
+ * - Main - program
+ *   Description -how to assisn numbers
+ */
+ int main(void)
+	int = n;
+
+{if n=0
+	printf("is zero");
+if n<0
+	printf("is negative");
+else
+	printf("is positive");
+return (0);
+}
+
