532a80594b69d3672d42f1e1e07f5fe210c49c07
Ntikui <veronicakorio@gmail.com>
2023-02-18 01:11:27 -0800

    0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * -main- program intergers
+ *  descriptions assign a number
+ *  return(0)
+ *  */
+
+int main (void)
+	int = n;
+{
+	if n>0
+		printf("is positive");
+	if n=0
+		printf("is zero");
+	if n<0
+		printf("is negative");
+	return (0);
+}
