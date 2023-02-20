65537521398bf26afda86340ef917474a0ce2d3d
Ntikui <veronicakorio@gmail.com>
2023-02-20 12:45:52 -0800

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,13 +7,14 @@
 * Return - (0)
 */
 int main(void)
-{
-	int n;
 
-	for (n = 0; n < 10; n++)
-	{
-		putchar(n);
-	}
-	putchar('\n');
-	return (0);
+{
+int n;
+
+for (n = 0; n < 10; n++)
+{
+putchar(n);
+}
+putchar('\n');
+return (0);
 }
