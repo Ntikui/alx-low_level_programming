90f56875599fc3f72d308348dd4d6e513e6d7aa9
Ntikui <veronicakorio@gmail.com>
2023-02-20 11:00:19 -0800

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,16 +1,17 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 /* 
  * Main:assign a random number to the variable n
  * Description- print the last digit of the number
  * betty style doc for function main goes there.
  */
 int main(void)
-(
+{
  int n;
-srand(time(0));
-n = rand() - RAND_MAX / 2;
+
+  srand(time(0));
+  n = rand() - RAND_MAX / 2;
 /* your code goes there */
 
 if ((n % 10) > 5)
