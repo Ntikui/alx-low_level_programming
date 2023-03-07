5b6a9b04436a636dd0ed2365c2266756aa14763e
Ntikui <veronicakorio@gmail.com>
2023-03-06 18:49:19 -0800

    0-memset.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,6 +1,16 @@
-#ifndef MAIN.H
+#ifndef MAIN_H
 #define MAIN_H
 
+/**
+ * File: main.h
+ * Auth: Mugambi
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x07-pointers_arrays_strings directory.
+ */
+
+#include <stddef.h>
+
+int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
@@ -9,4 +19,6 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
-#endif 
+void set_string(char **s, char *to);
+
+#endif /* MAIN_H */
